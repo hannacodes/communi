de943e72dac91d75054d284fd66fea2362c68f37
hannacodes <hannakoh12@gmail.com>
2022-04-10 00:17:25 -0400

    created data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+var threads = [
+    {
+        posthead: "To: Person in my English Class",
+        text: "You smiled at me when I sat down next to you, but I forgot ask for your number! Would love to be friends.",
+        author: "@KathyL"
+    }
+]
