12b7200284b61c9fc735400a329bed17e1ba81f9
hannacodes <hannakoh12@gmail.com>
2022-04-10 11:36:07 -0400

    added message popup window

--- data.js
+++ data.js
@@ -4,55 +4,65 @@ var threads = [
         text: "You smiled at me when I sat down next to you, but I forgot ask for your number! Would love to be friends.",
         author: "@KathyL",
         color:"blue", 
-        timestamp: "at 10:30 PM"
+        timestamp: "at 10:30 PM",
+        count:0
     }, 
     {
         title: "Boy at the Coffee shop near Pike's Place",
         text: "You were in front of me and ordered a Hot Chocolate and I asked you why you didn't drink coffee.",
         author: "@JohnD",
         color: "pink",
-        timestamp: "on April 1st"
+        timestamp: "on April 1st",
+        count:1
     },
     {
         title: "Girl who waved at me at the grocery store",
         text: "You seemed to think that I was someone you knew before and you looked sad when I didn't wave back. I'm sorry!! You seem like a really sweet person. ",
         author: "@BeckyD",
         color: "purple",
-        timestamp: "on April 3rd"
+        timestamp: "on April 3rd",
+        count:2
     },
     {
         title: "Boy wearing a bright pink hat",
         text: "That hat was atrocious but I commend your fashion choices and your bravery. Would love to meet you so I can burn that hat. ",
         author: "@ElizaL",
         color: "yellow",
-        timestamp: "on April 2nd"
+        timestamp: "on April 2nd",
+        count:3
     },
     {
         title: "Person who walked into lecture late",
         text: "Like, really late. I really want to know why you still went to lecture that day.",
         author: "@YoungJ",
         color: "yellow",
-        timestamp: "at 10:20 AM"
+        timestamp: "at 10:20 AM",
+        count:4
+        
     },
     {
         title: "Someone wearing sunglasses indoors",
         text: "I only have one question for you, why???? - the person who looked at you when you walked in. ",
         author: "@LucN",
         color: "blue",
-        timestamp: "on March 31st"
+        timestamp: "on March 31st",
+        count:5
     },
     {
         title: "The person bringing a teddy bear to class",
         text: "Was it bring your stuffed animal to class day? What class is this and where can I take it!",
         author: "@AlexS",
         color: "purple",
-        timestamp: "on March 31st"
+        timestamp: "on March 31st",
+        count:6
+        
     }, 
     {
         title: "The girl I saw thrifting a bright yellow shirt",
         text: "What are you going to do with that? I hope you're not just going to wear it as is...",
         author: "@SophieF",
         color: "blue",
-        timestamp: "on March 30th"
+        timestamp: "on March 30th",
+        count:7
     }, 
 ]
